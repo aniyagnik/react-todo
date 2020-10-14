fde09ccd2e565237d30f469d560cccf317ca494b
aniyagnik <ani789yagnik@gmail.com>
2020-10-14 22:36:56 +0530

    Updates

---
+++ precache-manifest.f4788af9f4219838988d362aad6821c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab73d65796f0524f6d12c8977f90f56",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "1c0cf821a2e2a4c21b75",
+    "url": "/react-todo/static/css/main.bd271c7d.chunk.css"
+  },
+  {
+    "revision": "3c237f341544fa82cabf",
+    "url": "/react-todo/static/js/2.a3e5e13f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo/static/js/2.a3e5e13f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c0cf821a2e2a4c21b75",
+    "url": "/react-todo/static/js/main.821b473e.chunk.js"
+  },
+  {
+    "revision": "819a54d715215b235721",
+    "url": "/react-todo/static/js/runtime-main.b9c8b86c.js"
+  }
+]);
